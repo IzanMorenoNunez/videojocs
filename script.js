f4225cd23e5eef3b8d8f6142930c437fd02ebfce
IzanMorenoNunez <izanmoreno@paucasesnovescifp.cat>
2025-04-08 13:09:47 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,38 @@
+document.addEventListener('mousemove', (e) => {
+    const linterna = document.getElementById('linterna');
+    linterna.style.background = `radial-gradient(circle at ${e.clientX}px ${e.clientY}px, rgba(255, 185, 8, 0.2) 0.5%, rgba(250, 165, 7, 0.13) 5%, rgba(244, 141, 6, 0.07) 10%, rgba(0, 0, 0, 0.99) 45%)`;
+});
+const $days = document.getElementById('days'),
+$hours = document.getElementById('hours'),
+$minutes = document.getElementById('minutes'),
+$seconds = document.getElementById('seconds'),
+$finalMessage = document.querySelector('.final-sms');
+
+//Fecha a futuro
+const countdownDate = new Date('04 20, 2025 12:48:00').getTime();
+
+let interval = setInterval(function(){
+    //Obtener fecha actual y milisegundos
+    const now = new Date().getTime();
+
+    //Obtener las distancias entre ambas fechas
+    let distance = countdownDate - now;
+
+    //Calculos a dias-horas-minutos-segundos
+    let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    let hours = Math.floor((distance % (1000 * 60 * 60 * 24 )) / (1000 * 60 * 60));
+    let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((distance % (1000 * 60 )) / (1000));
+
+    //Escribimos resultados
+    $days.innerHTML = days;
+    $hours.innerHTML = hours;
+    $minutes.innerHTML = minutes;
+    $seconds.innerHTML = ('0' + seconds).slice(-2);
+
+    //Cuando llegue a 0
+    if(distance < 0){
+        clearInterval(interval);
+        $finalMessage.style.transform = 'translateY(0)';
+    }
+}, 1000);
