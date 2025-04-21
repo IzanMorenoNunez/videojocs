c431b335dac3b2596c2431ce50ac766aa4aa87d3
IzanMorenoNunez <izanmoreno@paucasesnovescifp.cat>
2025-04-21 16:11:33 +0200

    Comentaris POST-GET

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ $seconds = document.getElementById('seconds'),
 $finalMessage = document.querySelector('.final-sms');
 
 //Fecha a futuro
-const countdownDate = new Date('04 20, 2025 12:48:00').getTime();
+const countdownDate = new Date('05 20, 2025 12:48:00').getTime();
 
 let interval = setInterval(function(){
     //Obtener fecha actual y milisegundos
